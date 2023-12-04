fbf86e9a7983e2f3fc2580e0daf5aa45e2bec471
Lane Simpson <lanesimpson14@gmail.com>
2023-12-04 16:26:59 -0500

    Submit work in progress for repairs form

--- app.py
+++ app.py
@@ -1,23 +1,18 @@
 from flask import Flask, render_template, request, redirect, url_for
 import mysql.connector
 
-app = Flask(__name__)
-
-@app.route('/')
-def index():
-    db_config = {
+db_config = {
     'host': 'dbdev.cs.kent.edu',
     'user': 'lsimps14',
     'password': 'of67DKid',
     'database': 'lsimps14',
-    }  
+}  
 
+app = Flask(__name__)
+
+@app.route('/')
+def index():
     connection = mysql.connector.connect(**db_config)
-
-    #connection._execute_query("INSERT INTO customers (customer_id, customer_name, customer_address, customer_email) VALUES(1, 'lane', '123 sesame st', 'lane@mail.com')")
-    #connection.commit()
-    #test line for commit
-    
     db_Info = connection.get_server_info()
     print("Connected to MySQL Server version ", db_Info)
     connection.close()
@@ -28,9 +23,6 @@ def dashboard():
     if request.method == 'POST':
         email = request.form.get('email')
         password = request.form.get('password')
-
-        # Add your authentication logic here
-        # For demonstration purposes, redirect to 'dashboard' without authentication
         return redirect(url_for('dashboard'))
 
     return render_template('dashboard.html')
@@ -45,89 +37,51 @@ def purchase_page():
 
 @app.route('/repairs')
 def repairs():
-    # In a real app, you would fetch past_repairs from your database
-    past_repairs = [
-        {"ticket_id": 1, "robot_id": 101, "condition": "Broken", "repair_location": "Repair Center", "technician": "John Doe", "part_id": "P123"},
-        # Add more repair entries as needed
-    ]
+    connection = mysql.connector.connect(**db_config)
+    cursor = connection.cursor(dictionary=True)
+
+    cursor.execute("SELECT * FROM Repairs")
+    past_repairs = cursor.fetchall()
     return render_template('repairs.html', past_repairs=past_repairs)
+
+@app.route('/submit_repair', methods=['POST'])
+def update_repair():
+    robot_id = request.form.get('robot_id')
+    condition = request.form.get('condition')
+    repair_location = request.form.get('repair_location')
+    technician = request.form.get('technician')
+    part_id = request.form.get('part_id')
+    ticket_id = request.form.get('ticket_id')
+
+    query = """
+        UPDATE Repairs
+        SET robot_id = %s, condition_type = %s, repair_location = %s, technician = %s, part_id = %s
+        WHERE ticket_id = %s
+    """
+    data = (robot_id, condition, repair_location, technician, part_id, ticket_id)
+
+    return execute_query(query, data)
+
+
+
+
+def execute_query(query, data=None):
+    db = mysql.connector.connect(**db_config)
+    cursor = db.cursor()
+
+    try:
+        cursor.execute(query, data)
+        db.commit()
+    except Exception as e:
+        db.rollback()
+        raise e
+    finally:
+        cursor.close()
+        db.close()
+
+    return "Query executed successfully"
+
+
 
 if __name__ == '__main__':
     app.run(debug=True, host='localhost', port=5006)
-
-# from flask import Flask, render_template, request, redirect, url_for
-# import mysql.connector
-
-# app = Flask(__name__)
-
-# # replace these with the database stuff for whoever is doing the database
-# db_config = {
-#     'host': 'dbdev.cs.kent.edu',
-#     'user': 'lsimps14',
-#     'password': 'of67DKid',
-#     'database': 'lsimps14'
-# }
-
-# def fetch_past_repairs():
-#     past_repairs = []
-#     connection = None
-#     try:
-#         connection = mysql.connector.connect(**db_config)
-#         cursor = connection.cursor(dictionary=True)
-
-#         cursor.execute("SELECT * FROM repairs")
-#         past_repairs = cursor.fetchall()
-
-#         return past_repairs
-
-#     except mysql.connector.Error as err:
-#         print(f"Error: {err}")
-#         return []
-
-#     finally:
-#         if connection and connection.is_connected():
-#             cursor.close()
-#             connection.close()
-    
-#     return past_repairs
-
-# @app.route('/')
-# def index():
-#     past_repairs = fetch_past_repairs()
-#     return render_template('orders.html', past_repairs=past_repairs)
-
-# @app.route('/submit_repair', methods=['POST'])
-# def submit_repair():
-#     robot_id = request.form['robot_id']
-#     condition = request.form['condition']
-#     repair_location = request.form['repair_location']
-#     technician = request.form['technician']
-#     part_id = request.form['part_id']
-
-#     add_repair_to_database(robot_id, condition, repair_location, technician, part_id)
-
-#     # Redirect to the home page after submitting the repair
-#     return redirect(url_for('index'))
-
-# def add_repair_to_database(robot_id, condition, repair_location, technician, part_id):
-#     try:
-#         connection = mysql.connector.connect(**db_config)
-#         cursor = connection.cursor()
-
-#         add_repair_query = "INSERT INTO repairs (robot_id, condition, repair_location, technician, part_id) " \
-#         "VALUES (%s, %s, %s, %s, %s)"
-#         cursor.execute(add_repair_query, (robot_id, condition, repair_location, technician, part_id))
-#         connection.commit()
-
-#         print("processing...")
-
-#     except mysql.connector.Error as err:
-#         print(f"Error: {err}")
-
-#     finally:
-#         if connection.is_connected():
-#             cursor.close()
-#             connection.close()
-
-# if __name__ == '__main__':
-#     app.run(debug=True)
